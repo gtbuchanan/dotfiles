09cf4df0e65c7541900744043c501a113a21ddf3
Taylor Buchanan <taylor.buchanan@ewn.com>
2025-05-02 12:15:49 -0500

    Merge EWN config

--- .chezmoitemplates/powershell_profile.ps1
+++ .chezmoitemplates/powershell_profile.ps1
@@ -1,10 +1,25 @@
-{{- if eq .chezmoi.os "windows" -}}
-oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/cobalt2.omp.json | Invoke-Expression
-
-{{ end -}}
+# Configure aliases
 Set-Alias -Name c -Value clear
 Set-Alias -Name cm -Value chezmoi
 Set-Alias -Name g -Value git
 Set-Alias -Name pn -Value pnpm
+{{- if eq .hosttype "ewn" }}
+Set-Alias -Name tg -Value TortoiseGitProc
+{{- end }}
 
 Function cmcd { cd (cm source-path) }
+
+# Configure Oh My Posh
+$env:POSH_THEME = "$env:POSH_THEMES_PATH/cobalt2.omp.json"
+oh-my-posh init pwsh | Invoke-Expression
+
+# Configure posh-git
+Import-Module posh-git
+
+{{- if eq .hosttype "ewn" }}
+# Configure PSRSA module
+Import-Module -Name $env:USERPROFILE/Code/PSRSA/src/PSRSA.psm1
+
+# Configure JIRA CLI
+$env:JIRA_API_TOKEN = {{ (index (dashlanePassword "JIRA API Token") 0).password | quote }}
+{{- end }}
