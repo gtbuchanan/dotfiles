55d5220561dd914fd254e43929e71159b50370e8
Taylor Buchanan <me+dev.git@taylorbuchanan.com>
2025-12-12 01:44:00 -0600

    Add fuzzy chords for Git on Windows

--- home/.chezmoitemplates/powershell_profile.ps1
+++ home/.chezmoitemplates/powershell_profile.ps1
@@ -65,8 +65,35 @@ Set-PSReadLineOption -EditMode Vi -ViModeIndicator Script -ViModeChangeHandler {
 Import-Module PowerShellGet
 
 # Configure PSFzf
-Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Import-Module PSFzf
+Set-PSReadLineKeyHandler `
+  -Key Tab `
+  -BriefDescription 'Fzf Tab Completion' `
+  -Description 'Autocomplete commands via fzf' `
+  -ScriptBlock { Invoke-FzfTabCompletion }
+function local:Write-AtCursor ([Parameter(ValueFromPipeline)]$Result) {
+  If ($Result.Length -gt 0) {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($Result -join "")
+  }
+  [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+}
+# Define simpler chords than `GitKeyBindings`
+Set-PSReadLineKeyHandler -Chord 'Ctrl+g,b' -ScriptBlock {
+  Invoke-PsFzfGitBranches | Write-AtCursor
+}
+Set-PSReadLineKeyHandler -Chord 'Ctrl+g,f' -ScriptBlock {
+  Invoke-PsFzfGitFiles | Write-AtCursor
+}
+# PSFzf tries Ctrl+g,Ctrl+h for this, but it doesn't actually work (likely due to PSReadLine)
+Set-PSReadLineKeyHandler -Chord 'Ctrl+g,h' -ScriptBlock {
+  Invoke-PsFzfGitHashes | Write-AtCursor
+}
+Set-PSReadLineKeyHandler -Chord 'Ctrl+g,s' -ScriptBlock {
+  Invoke-PsFzfGitStashes | Write-AtCursor
+}
+Set-PSReadLineKeyHandler -Chord 'Ctrl+g,t' -ScriptBlock {
+  Invoke-PsFzfGitTags | Write-AtCursor
+}
 Set-PsFzfOption `
   -PSReadlineChordProvider 'Ctrl+t' `
   -PSReadlineChordReverseHistory 'Ctrl+r' `
